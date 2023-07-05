708bcd56f8b243d5a4dd44819363cc88fe21ae7e
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2023-07-05 18:20:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-
 const cols = 3;
 const main = document.getElementById('main');
 let parts = [];
+
 
 let images = [
   "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
